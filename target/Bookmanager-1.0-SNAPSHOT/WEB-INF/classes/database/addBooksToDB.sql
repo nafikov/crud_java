b100e6fc577471307e88066953106e043eb34a91
sparrow90 <akmakk@yandex.ru>
2017-04-07 15:33:25 +0500

    first commit

---
+++ target/Bookmanager-1.0-SNAPSHOT/WEB-INF/classes/database/addBooksToDB.sql
@@ -0,0 +1,9 @@
+INSERT INTO book (title, author, price) VALUES ('1984', 'Джордж Оруэлл', 590);
+INSERT INTO book (title, author, price) VALUES ('Война и мир', 'Лев Толстой', 350);
+INSERT INTO book (title, author, price) VALUES ('Лолита', 'Владимир Набоков', 450.5);
+INSERT INTO book (title, author, price) VALUES ('Убить пересмешника', 'Харпер Ли', 390);
+INSERT INTO book (title, author, price) VALUES ('Гамлет', 'Уильям Шекспир', 149.50);
+INSERT INTO book (title, author, price) VALUES ('Приключения Гекльберри Финна', 'Марк Твен', 249);
+INSERT INTO book (title, author, price) VALUES ('Путешествия Гулливера', 'Джонатан Свифт', 333);
+INSERT INTO book (title, author, price) VALUES ('Над пропастью во ржи', 'Джером Д. Сэлинджер', 195);
+INSERT INTO book (title, author, price) VALUES ('Унесенные ветром', 'Маргарет Митчелл', 99.5);
